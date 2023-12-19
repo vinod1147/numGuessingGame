3e4ba59c11c2e3a40a1e73ea461848fbc83ce1fc
Vinod <72577940+vinod1147@users.noreply.github.com>
2023-12-19 22:55:40 +0530

    done v1

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let submit = document.querySelector('.submit')
 const msg = document.querySelector('.msg')
 const prevGuess = document.querySelector('.prevGuess')
 const warn = document.querySelector('.warn')
+const remainChance = document.querySelector('.remainChance')
 
 const randomNum = Math.round(Math.random() * 1000)
 
@@ -26,8 +27,29 @@ submit.addEventListener('click', function (e) {
 
     includeCheck()
 
+    restart()
+
+    lessThanGreaterCheck()
 
 })
+
+function lessThanGreaterCheck() {
+    if (num.value > 1000) {
+        warn.innerHTML = "number is less than 1000"
+    } else if (num.value < 0) {
+        warn.innerHTML = "number is more than 0"
+    }
+}
+
+function restart() {
+    if (num.value == randomNum) {
+        submit.innerHTML = 'restart'
+        console.log("submit")
+        prevNum = []
+        num.value = ''
+        location.reload()
+    }
+}
 
 function includeCheck() {
     if (prevNum.includes(prevNum[0])) {
@@ -36,7 +58,7 @@ function includeCheck() {
 }
 
 function emptyNumCheck() {
-    if (num.value === "") {
+    if (num.value === "" && prevNum.length === 0) {
         warn.innerHTML = "pls enter a number"
         msg.innerHTML = ''
         prevNum = []
